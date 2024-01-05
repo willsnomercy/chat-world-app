50e592b344e64b826cef618a05d0a6fc6ab0647b
ApalahXy <155531891+ApalahXy@users.noreply.github.com>
2024-01-05 19:04:13 +0700

    Update config.js

--- data/js/config.js
+++ data/js/config.js
@@ -1,11 +1,10 @@
-export const firebaseConfig = {apiKey: "AIzaSyAHp2TlJiZ_Fbttq9M_Qd9lG7c64_J62sM",
-    apiKey: "AIzaSyAv0ppMD3HFlVxlQL5MtscWKmIAtAxmhYA",
-    authDomain: "chat-app--world-chat.firebaseapp.com",
-    databaseURL: "https://chat-app--world-chat-default-rtdb.firebaseio.com",
-    projectId: "chat-app--world-chat",
-    storageBucket: "chat-app--world-chat.appspot.com",
-    messagingSenderId: "872704705411",
-    appId: "1:872704705411:web:04e52291bcb4a44cdd2cb6",
-    measurementId: "G-XN29N56E58"
+export const firebaseConfig = {
+  apiKey: "AIzaSyDgrdAERzCNZ3LWzUiXMOh5Rz_fAAXJtAY",
+  authDomain: "chat-world-app1.firebaseapp.com",
+  projectId: "chat-world-app1",
+  storageBucket: "chat-world-app1.appspot.com",
+  messagingSenderId: "1008210775353",
+  appId: "1:1008210775353:web:b86c905fd62cfe1a776ce8",
+  measurementId: "G-ZFX303XZ58"
     
 };
