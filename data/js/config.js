f9a51abf1de8571e804b89a543804a8d76701aed
ApalahXy <155531891+ApalahXy@users.noreply.github.com>
2024-01-07 01:44:11 +0700

    Add files via upload

--- data/js/config.js
+++ data/js/config.js
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyD8Pfw4bqqA9HrKmbBmJxlOV9gBs1GfJTQ",
-  authDomain: "app-chat-worlds.firebaseapp.com",
-  databaseURL: "https://app-chat-worlds-default-rtdb.firebaseio.com",
-  projectId: "app-chat-worlds",
-  storageBucket: "app-chat-worlds.appspot.com",
-  messagingSenderId: "453936279604",
-  appId: "1:453936279604:web:f8828686f8186f0bb59a61"
+  apiKey: "AIzaSyA5fTNlsrYJO1AqtPj2j0BNL5GHShyPPKM",
+  authDomain: "apps-chat-worlds.firebaseapp.com",
+  databaseURL: "https://apps-chat-worlds-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "apps-chat-worlds",
+  storageBucket: "apps-chat-worlds.appspot.com",
+  messagingSenderId: "395735317936",
+  appId: "1:395735317936:web:40d5ff89ddbea60f082a75",
+    
 };
