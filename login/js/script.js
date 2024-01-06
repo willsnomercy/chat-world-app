5f4e31302e789e86cb0995b9323470ce1ae73eba
ApalahXy <155531891+ApalahXy@users.noreply.github.com>
2024-01-07 00:36:26 +0700

    Update script.js

--- login/js/script.js
+++ login/js/script.js
@@ -1,5 +1,6 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
-import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithRedirect} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { getAuth, onAuthStateChanged, getRedirectResult, GoogleAuthProvider, GithubAuthProvider, FacebookAuthProvider, signInWithRedirect, sendSignInLinkToEmail } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 import { firebaseConfig } from '../../data/js/config.js';
 
 (() => {
@@ -29,6 +30,10 @@ import { firebaseConfig } from '../../data/js/config.js';
                     <i class="fa-brands fa-google"></i>
                     <p>Google</p>
                 </button>
+                <button class="c-green" id="email-form">
+                    <i class="fa-regular fa-envelope"></i>
+                    <p>Email</p>
+                </button>
             </div>
         `);
 
@@ -40,9 +45,12 @@ import { firebaseConfig } from '../../data/js/config.js';
         }
 
         const google = card.querySelector('#google-login');
+        const github = card.querySelector('#github-login');
+        const facebook = card.querySelector('#facebook-login');
+        const emailForm = card.querySelector(`#email-form`);
 
         const visibility = (clicked) => {
-            let providerAll = [google];
+            let providerAll = [google, github, facebook, emailForm];
             providerAll.forEach((element) => {
                 if(clicked !== element) {
                     element.style.visibility = 'hidden';
@@ -51,7 +59,7 @@ import { firebaseConfig } from '../../data/js/config.js';
 
             card.querySelector('#changeLang').style.visibility = 'hidden';
 
-            let loading = ['Loading...'];
+            let loading = ['Okay Let\'s Go', 'Wise Choice', 'Wkwkwk', 'Not Bad!', 'Ur good to go!'];
             loading = loading[Math.floor(Math.random() * loading.length)];
 
             clicked.querySelector('p').innerHTML = loading;
@@ -63,9 +71,94 @@ import { firebaseConfig } from '../../data/js/config.js';
             const provider = new GoogleAuthProvider();
             signInWithRedirect(auth, provider);
         }
+        github.onclick = () => {
+            visibility(github);
+            const provider = new GithubAuthProvider();
+            signInWithRedirect(auth, provider);
+        }
+        facebook.onclick = () => {
+            popup.confirm({
+                msg: 'We <b class="c-red">ARE NOT</b> recommending you to login with Facebook since Meta <b class="c-red">DOES NOT</b> care about privacy.<br/>Still want to continue!?',
+                onyes: () => {
+                    const provider = new FacebookAuthProvider();
+                    signInWithRedirect(auth, provider);
+                    visibility(facebook);
+                }
+            })
+        }
+
+        emailForm.addEventListener('click', () => transition(card, emailCard));
 
         container.innerHTML = '';
         container.appendChild(card);
+    }
+    
+    const emailCard = () => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = (`
+            <h1>${lang.login}</h1>
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" placeholder="${lang.your_email}.." maxlength="360"/>
+            <div class="group-flex">
+                <button id="back-to-login" class="c-red">${lang.cancel}</button>
+                <button id="email-login" class="c-green">${lang.send}</button>
+            </div>
+        `);
+        
+        const loginForm = card.querySelector(`#back-to-login`);
+        loginForm.addEventListener('click', () => transition(card, loginCard));
+
+        const input = card.querySelector(`#email`);
+        const send = card.querySelector(`#email-login`);
+        send.onclick = () => {
+            emailHandler(input.value);
+            send.innerHTML = `${lang.checking}...`;
+            input.style.visibility = 'hidden';
+            loginForm.style.visibility = 'hidden';
+        };
+
+        const emailHandler = (email) => {
+            const actionCodeSettings = {
+                url: `${window.location.origin}/login/email-verification.html`,
+                handleCodeInApp: true,
+            };
+            sendSignInLinkToEmail(auth, email, actionCodeSettings)
+            .then(() => {
+                window.localStorage.setItem('kiriminEmailSignIn', email);
+                popup.alert({
+                    msg: lang.signin_link,
+                    type: "blue",
+                    onyes: () => transition(card, almostCard)
+                });
+            })
+            .catch((error) => {
+                popup.alert({msg: error, onyes: () => transition(card, emailCard)});
+            });
+        }
+
+        container.appendChild(card);
+        input.focus();
+    }
+
+    const almostCard = () => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = (`
+            <h1>${lang.almost}!</h1>
+            <p><i>${lang.we_sent}</i></p>
+            <p class="c-yellow"><i>${lang.do_not_close}</i></p>
+        `);
+
+        container.appendChild(card);
+    }
+
+    const transition = (prevCard, nextCard) => {
+        prevCard.classList.add('deleted');
+        setTimeout(() => {
+            container.removeChild(prevCard);
+            nextCard();
+        }, 200);
     }
 
     const langCheker = async() => {
